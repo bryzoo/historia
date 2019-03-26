6860e92f8dc527783e8e5648beb1a44b51ce35bc
Artur Bryzek <arturbryzek@MacBook-Air-Artur-2.local>
2019-03-26 20:54:45 +0100

    aa

--- src/app/tab4/tab4.page.spec.ts
+++ src/app/tab4/tab4.page.spec.ts
@@ -1,6 +1,5 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { Tab4Page } from './tab4.page';
 
 describe('Tab4Page', () => {
