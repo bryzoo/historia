708e2722a78d17e83f6ee42e6bcead4a68bce885
Artur Bryzek <arturbryzek@MacBook-Air-Artur-2.local>
2019-03-28 19:01:57 +0100

    a

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,8 @@ import { Component } from '@angular/core';
   template: `
     <ion-slides pager="true" scrollbar="true" [options]="slideOpts">
       <ion-slide>
-        <h1>Slide 1</h1>
+      <ion-img [src]="Slide1.png"></ion-img>
+        <h1>Poznaj historię Polski od Piastów do współczesności</h1>
       </ion-slide>
       <ion-slide>
         <h1>Slide 2</h1>
