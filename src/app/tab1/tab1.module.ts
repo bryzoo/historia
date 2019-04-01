f7c85ad162716d1221f2740de2f9e4dba6238cf0
Artur Bryzek <arturbryzek@MacBook-Air-Artur-2.local>
2019-04-01 20:50:41 +0200

    tak

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -4,9 +4,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
-import { Component } from '@angular/core';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
 
 
 @NgModule({
